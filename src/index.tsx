cd0bd70f519b29a096bfbe1d2f589a9d914994d8
PrinceRathore18 <prince8368191294@gmail.com>
2026-05-27 09:37:57 +0000

    fix full homepage render

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
-import Navbar from "./Navbar";
-import Hero from "./Hero";
+import Navbar from "./Navbar.tsx";
+import Hero from "./Hero.tsx";
+import Skills from "./Skills.tsx";
+import Program from "./Program.tsx";
+import Team from "./Team.tsx";
+import Contact from "./Contact.tsx";
+import GetInvolved from "./GetInvolved.tsx";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <main>
+        <Hero />
+        <Skills />
+        <Program />
+        <Team />
+        <GetInvolved />
+        <Contact />
+      </main>
     </>
   );
 }
