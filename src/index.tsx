e070a935b77ba894ed464294e89a1fe34470c02a
PrinceRathore18 <prince8368191294@gmail.com>
2026-05-27 09:39:30 +0000

    fix root component imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,21 @@
-import Navbar from "./Navbar.tsx";
-import Hero from "./Hero.tsx";
-import Skills from "./Skills.tsx";
-import Program from "./Program.tsx";
-import Team from "./Team.tsx";
-import Contact from "./Contact.tsx";
-import GetInvolved from "./GetInvolved.tsx";
+import Navbar from "../Navbar";
+import Hero from "../Hero";
+import Skills from "../Skills";
+import Program from "../Program";
+import Team from "../Team";
+import Contact from "../Contact";
+import GetInvolved from "../GetInvolved";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <main>
-        <Hero />
-        <Skills />
-        <Program />
-        <Team />
-        <GetInvolved />
-        <Contact />
-      </main>
+      <Hero />
+      <Skills />
+      <Program />
+      <Team />
+      <GetInvolved />
+      <Contact />
     </>
   );
 }
