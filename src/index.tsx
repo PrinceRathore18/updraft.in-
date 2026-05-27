8c89b527f1d7be511f3e5d0ae8efa00f7efaced7
PrinceRathore18 <prince8368191294@gmail.com>
2026-05-27 09:21:10 +0000

    restore full homepage

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
-import Home from "./Home";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Skills from "./Skills";
+import Program from "./Program";
+import Team from "./Team";
+import Contact from "./Contact";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Program />
+      <Team />
+      <Contact />
+    </>
+  );
 }
