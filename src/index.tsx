495b42124cfe2ff1a1ea55b51c182bbd00811b77
PrinceRathore18 <prince8368191294@gmail.com>
2026-05-27 09:23:45 +0000

    fix broken imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
-import Navbar from "./Navbar";
-import Hero from "./Hero";
-import Skills from "./Skills";
-import Program from "./Program";
-import Team from "./Team";
-import Contact from "./Contact";
+import Hero from "../Hero";
+import Navbar from "../Navbar";
+import Contact from "../Contact";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Skills />
-      <Program />
-      <Team />
       <Contact />
     </>
   );
